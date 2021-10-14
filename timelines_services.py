af3b2f8172380c819c6f8d975c0bb5b5b4bb9428
kimvinh <42789132+kimvinh@users.noreply.github.com>
2021-10-14 15:10:43 -0700

    Add files via upload

--- timelines_services.py
+++ timelines_services.py
@@ -8,6 +8,9 @@ import logging.config
 db = sqlite_utils.Database("posts.db")
 
 # Using Routes
+@hug.get("/post/")
+def posts():
+    return {"post": db["post"].rows}
 
 ''' Design the service of user timeline '''
 @hug.get("/userTimeline/{username}")
@@ -22,10 +25,17 @@ def retrieveUserTimeline(response, username: hug.types.text):
     return {"post": posts}
 
 ''' Design the service of home timeline '''
-@hug.post("/homeTimeline/{username}")
-def retrieveHomeTimeline(response):
+@hug.get("/homeTimeline/{username}")
+def retrieveHomeTimeline(response, username: hug.types.text):
+    posts = []
+    try:
+        for post in db.query("SELECT * from post WHERE username IN (SELECT following FROM follow WHERE username = ?) ORDER BY timestamp DESC",
+                             [username]):
+            posts.append(post)
+    except sqlite_utils.db.NotFoundError:
+        response.status = '404 NOT FOUND'
 
-    return
+    return {"post": posts}
 
 ''' Design the service of public timeline '''
 @hug.get("/publicTimeline/")
@@ -39,11 +49,9 @@ def retrievePublicTimeline(response):
 
     return {"post": posts}
 
+''' Design the service of allowing an existing user to post a message '''
 @hug.post("/post/", status=hug.falcon.HTTP_201)
-def postMessage(response,
-		username: hug.types.text,
-		text: hug.types.text,
-		):
+def postMessage(response, username: hug.types.text, text: hug.types.text):
 
     posts = db["post"]
 
@@ -53,10 +61,20 @@ def postMessage(response,
         "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
 
+    foundUsername = False
+    for usern in db.query("SELECT username FROM post GROUP BY username"):
+        if usern["username"] == username:
+            foundUsername = True
+            break;
+
+    if foundUsername == False:
+        response.status = hug.falcon.HTTP_401
+        return {"error": f"You are unauthorized to post a message because '{username}' is invalid."}
+
     try:
         posts.insert(post)
     except Exception as e:
-        response.status = hug.falcon.HTTP_409
+        response.status = hug.falcon.HTTP_400
         return {"error": str(e)}
 
     response.set_header("Location", "/post/")
