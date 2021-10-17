7fd9cb4be2ea139f3884dc1b8b27038380365330
QuangDNguyen2211 <70936038+QuangDNguyen2211@users.noreply.github.com>
2021-10-16 22:44:11 -0700

    Add files via upload

--- api/api.py
+++ api/api.py
@@ -1,9 +1,3 @@
-# Science Fiction Novel API - Hug Edition
-#
-# Adapted from "Creating Web APIs with Python and Flask"
-# <https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask>.
-#
-
 import configparser
 import logging.config
 
@@ -24,11 +18,9 @@ def sqlite(section="sqlite", key="dbfile", **kwargs):
     dbfile = config[section][key]
     return sqlite_utils.Database(dbfile)
 
-
 @hug.directive()
 def log(name=__name__, **kwargs):
     return logging.getLogger(name)
-
 
 # Routes
 #
@@ -36,92 +28,116 @@ def log(name=__name__, **kwargs):
 def books(db: sqlite):
     return {"users": db["users"].rows}
 
-@hug.get("/users/{id}")
-def retrieve_book(response, id: hug.types.number, db: sqlite):
+@hug.get("/users/{username}")
+def retrieve_book(response, username: hug.types.text, db: sqlite):
     users = []
     try:
-        user = db["users"].get(id)
-        user["follows"] = "me"
+        user = db["users"].get(username)
         users.append(user)
-        # println(user['follows'])
     except sqlite_utils.db.NotFoundError:
         response.status = hug.falcon.HTTP_404
     return {"users": user}
 
 # Create new user
-@hug.post("/users/", status=hug.falcon.HTTP_201)
+@hug.post("/create/", status=hug.falcon.HTTP_201)
 def create_user(
     response,
     username: hug.types.text,
     email_address: hug.types.text,
     password: hug.types.text,
     bio: hug.types.text,
-    follows: hug.types.text,
-    db: sqlite,
-):
-    users = db["users"]
+    db: sqlite):
 
+    users_db = db["users"]
     user = {
         "username": username,
         "email_address": email_address,
         "password": password,
-        "bio": bio,
-        "follows": follows,
+        "bio": bio
     }
 
     try:
-        users.insert(user)
-        user["id"] = users.last_pk
+        users_db.insert(user)
     except Exception as e:
         response.status = hug.falcon.HTTP_409
         return {"error": str(e)}
 
-    response.set_header("Location", f"/users/{user['id']}")
+    # response.set_header("Location", f"/users/{user['id']}")
     return user
 
-# Follow new user
-@hug.get("/follow/username")
-def add_follow(response, username: hug.types.text, db: sqlite):
-    users = []
+#Change Password
+@hug.post("/change_password/", status=hug.falcon.HTTP_200)
+def change_password(
+    response,
+    username: hug.types.text,
+    old_password: hug.types.text,
+    new_password: hug.types.text,
+    db: sqlite):
+
+    users_db = db["users"]
     try:
-        user = db["users"].get(username)
-        # user['follows'] += follow
-        users.append(user)
-        println(user['follows'])
+        user = users_db.get(username)
+        if old_password == user["password"]:
+            users_db.update(username, {"password": new_password})
+        else:
+            response.status = hug.falcon.HTTP_401
     except sqlite_utils.db.NotFoundError:
         response.status = hug.falcon.HTTP_404
-    return {"users": user}
 
-# # Unfollow user
-# @hug.post("/unfollow", status=hug.falcon.HTTP_201)
-# def unfollow():
+#Check password
+@hug.get("/check_password")
+def check_password(request, response, db: sqlite, status=hug.falcon.HTTP_200):
+
+    users_db = db["users"]
+    try:
+        user = users_db.get(request.params["username"])
+        if request.params["password"] != user["password"]:
+            response.status = hug.falcon.HTTP_401
+    except sqlite_utils.db.NotFoundError:
+        response.status = hug.falcon.HTTP_404
+    return response.status
+
+
+# Follow new user
+@hug.post("/follow/", status=hug.falcon.HTTP_201)
+def add_follow(
+    response,
+    username: hug.types.text,
+    following: hug.types.text,
+    db: sqlite):
+
+    try:
+        db["follows"].insert({"username": username,"following": following})
+    except sqlite_utils.db.NotFoundError:
+        response.status = hug.falcon.HTTP_404
+        return {"error": str(e)}
+
+    return {"username": username,"following": following}
+
+# Unfollow user
+@hug.post("/unfollow/", status=hug.falcon.HTTP_200)
+def unfollow(
+    response,
+    username: hug.types.text,
+    following: hug.types.text,
+    db: sqlite):
+
+    try:
+        db["follows"].delete((username, following))
+    except sqlite_utils.db.NotFoundError:
+        response.status = hug.falcon.HTTP_404
+        return {"error": str(e)}
 
 # Change bio
+@hug.post("/change_bio/", status=hug.falcon.HTTP_200)
+def change_bio(
+    response,
+    username: hug.types.text,
+    bio: hug.types.text,
+    db: sqlite):
 
-# Search
-@hug.get(
-    "/search",
-    examples=[
-        "published=2017",
-        "author=Heinlein",
-        "title=Star",
-        "first_sentence=night",
-    ],
-)
-def search(request, db: sqlite, logger: log):
-    users = db["users"]
-
-    conditions = []
-    values = []
-
-    for column in ["username", "email_address", "password", "bio", "follows"]:
-        if column in request.params:
-            conditions.append(f"{column} LIKE ?")
-            values.append(f"%{request.params[column]}%")
-
-    if conditions:
-        where = " AND ".join(conditions)
-        logger.debug('WHERE "%s", %r', where, values)
-        return {"users": users.rows_where(where, values)}
-    else:
-        return {"users": users.rows}
+    try:
+        db["users"].update(username, {"bio": bio})
+    except Exception as e:
+        response.status = hug.falcon.HTTP_404
+        return {"error": str(e)}
